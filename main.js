65eb138946700b4c1f748d7545275079e0e1b2a3
Marlon Fernandes <115473116+marlonfrnds@users.noreply.github.com>
2024-03-23 16:24:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+//1
+console.log('Hello World!')
+
+//2
+let isNumber = 2024
+console.log(typeof isNumber
+    ? `O valor inserido (${isNumber}) é um número`
+    : `O valor inserido (${isNumber}) não é um número` 
+    )
+
+//3
+let isString = 'John Doe'
+console.log(typeof isString
+    ? `O valor inserido (${isString}) é uma string`
+    : `O valor inserido (${isString}) não é uma string`
+    )
+
+//4
+let isBoolean = true
+console.log(typeof isBoolean
+    ? `O valor inserido (${isBoolean}) é um booleano`
+    : `O valor inserido (${isBoolean}) não é um booleano`  
+)
+
+//5
+let numberOne = 5
+let numberTwo = 5
+let sum = numberTwo + numberOne
+console.log(`A soma de ${numberTwo} + ${numberOne} é ${sum}`)
+
+//6
+let sub = numberOne - numberTwo
+console.log(`A subtração de ${numberOne} - ${numberTwo} é ${sub}`)
+
+//7
+let multi = numberOne * numberTwo
+console.log(`A multiplicação de ${numberOne} * ${numberTwo} é ${multi}`)
+
+//8
+let div = numberOne * numberTwo
+console.log(`A divisão de ${numberOne} / ${numberTwo} é ${div}`)
+
+//9
+let isPar = 2
+console.log(isPar % 2 == 0
+    ? `O valor inserido (${isPar}) é par`
+    : `O valor inserido (${isPar}) não é par`
+    )
+
+//10
+let isImpar = 3
+console.log(isImpar % 2 !== 0 
+    ? `O valor inserido (${isImpar}) é impar`
+    : `O valor inserido (${isImpar}) não é impar`  
+)
